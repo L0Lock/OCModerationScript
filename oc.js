a3b74d73300c6c2191155c90809559bec1108bd0
benzouye <benzouye@hotmail.com>
2018-03-02 18:12:02 +0100

    Refonte globale
    Meilleur traitement des promises et gestion asynchrone respectée sur le retrait des alertes et la fermeture du sujet

--- oc.js
+++ oc.js
@@ -1,25 +1,16 @@
 	// ==UserScript==
 	// @name	OC Moderation Script
-	// @author	Sakuto, L0Lock, benzouye
+	// @author	Sakuto, -L0Lock-, benzouye
 	// @namespace   http://ramelot-loic.be
-	// @description Rendre la modération d'OC plus facile
+	// @description Make the moderation easiest
 	// @include	*openclassrooms.com/*
-	// @version	1.0.4
+	// @version	1.1
 	// @grant	GM_xmlhttpRequest
 	// @grant	GM_getValue
 	// @grant	GM_setValue
 	// @require	http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js
 	// ==/UserScript==
-
-	const gitUser = "L0Lock";
-	const baseUri = "https://openclassrooms.com";
-	const answerFileLink = "https://raw.githubusercontent.com/"+gitUser+"/OCModerationScript/master/ocreply.json";
-	const loadingGif = "https://raw.githubusercontent.com/"+gitUser+"/OCModerationScript/master/loader.gif";
-
-	const answerFileIndex = "answers";
-	const answerFileLastFetchIndex = "answersLastFetch";
-	const delayBetweenConfigurationFetch = 86400000;
-
+	
 	// Liste des forums hiérarchisée
 	const forums = {
 		"Site web" : [
@@ -88,125 +79,113 @@
 			"Forum du staff"
 		]
 	};
-	var messages = GM_getValue(answerFileIndex).answers;
-	var messagesSection = getMessageBySection( messages, $('span[itemprop="title"]').last().text() );
-
-	if( messagesSection.length ) {
-		// Eléments et styles
-		$("#myFollowedThreads").after("<li><a href=\"#\" id=\"updateReply\">Mettre à jour les réponses</a></li>");
-		$(".nav-tabs--searchField").css( {"width": "40%"} );
-		$("input[name=submit_comment]").before( '<div id="oc-mod-panel"><h2 id="oc-mod-panel-title">Outils de modération</h2><div id="oc-mod-reponses"><h3 id="oc-mod-reponses-title">Messages possibles</h3></div><div id="oc-mod-options"><h3 id="oc-mod-options-title">Options</h3></div><div id="oc-mod-valid"></div></div>' );
-		$("#oc-mod-panel").css( {"float":"left","width":"75%","padding":"10px","border":"1px solid #4f8a03","border-radius":"5px"} );
-		$("#oc-mod-reponses").css( {"float":"left","width":"50%"} );
-		$("#oc-mod-options").css( {"float":"left","width":"50%"} );
-		$("#oc-mod-valid").css( {"float":"left","width":"100%","text-align":"center"} );
-		$("#oc-mod-panel-title").css( {"color":"#4f8a03","font-weight":"bold","line-height":"1em","margin-bottom":"10px"} );
-		$("#oc-mod-reponses-title").css( {"color":"#000","font-weight":"bold","line-height":"1em"} );
-		$("#oc-mod-options-title").css( {"color":"#000","font-weight":"bold","line-height":"1em"} );
-		$("#oc-mod-options").append( '<input name="hasHeader" type="checkbox" value="1" /> Ajouter entête de réponse<br />' );
-		$("#oc-mod-options").append( '<input name="shouldLock" type="checkbox" value="1" /> Fermer le sujet 🔒<br />' );
-		$("#oc-mod-options").append( '<input name="postMessage" type="checkbox" checked="checked" value="1" /> Poster le message directement<br />' );
-		$("#oc-mod-options").append( '<input name="dismissAlerts" type="checkbox" value="1" /> Retirer les alertes<br />' );
-		$("#oc-mod-valid").append( '<a id="oc-mod-validation" class="btn btn-primary">Modérer</a>' );
-		$("#oc-mod-validation").css( {"margin":"10px 0 0 5px","border":"1px solid #380e00","box-shadow":"inset 0 1px 1px 0 #a95f47","background-color":"#691c02","background-image":"linear-gradient(to bottom,#872403 0,#763019 49%,#691c02 50%,#421100 100%)","text-shadow":"0 -1px 0 #1c181b","text-decoration":"none"} );
-        $("#oc-mod-reponses").append('<img id="oc-mod-loading" src="'+loadingGif+'" />');
-
-		// Ajout des messages possibles si config ok
-		getConfigurationFile(false).then(() => {
-            $("#oc-mod-loading").hide();
-			for(var message of messagesSection ) {
-				var cases = '<input class="oc-mod-checkboxes" type="checkbox" value="'+message.id+'" /> '+message.title+'<br />';
-				$("#oc-mod-reponses").append( cases );
-			}
-		});
+	
+	// URL et chemins
+	const baseUri = "https://openclassrooms.com";
+	const answerFileLink = "https://raw.githubusercontent.com/L0Lock/OCModerationScript/master/ocreply.json";
+	
+	// Fermeture du sujet si demandée
+	if( GM_getValue( "threadToLock" ) != '' && GM_getValue( "threadToLock" ) !== undefined ) {
+		promiseRequest("GET", GM_getValue( "threadToLock" ) )
+			.then(() => GM_setValue( "threadToLock", '' ) );
 	}
+	
+	// Lien MAJ réponses avec gif loading
+	$(".nav-tabs--searchField").css( {"width": "40%"} );
+	$("#myFollowedThreads").after('<li><a href="#" id="oc-mod-update">Mettre à jour les réponses</a></li>');
 
+	// Initialisation variables
+	var configuration = [];
+	var messages = [];
+	
+	// Récupération des messages possibles
+	getConfigurationFile( false ).then( init() );
+	
+	/**
+	 * Traite les messages possibles
+	 * Affiche les éléments graphique de modération
+	 *
+	 */
+	function init() {
+		configuration = GM_getValue("answers").configuration;
+		messages = GM_getValue("answers").answers;
+		let messagesSection = getMessageBySection( messages, $('span[itemprop="title"]').last().text() );
+		
+		// Eléments et styles
+		if( messagesSection.length ) {
+			$("input[name=submit_comment]").before( '<div id="oc-mod-panel"><h2 id="oc-mod-panel-title">Outils de modération</h2><div id="oc-mod-reponses"><h3 id="oc-mod-reponses-title">Messages possibles</h3></div><div id="oc-mod-options"><h3 id="oc-mod-options-title">Options</h3></div><div id="oc-mod-valid"></div></div>' );
+			$("#oc-mod-panel").css( {"float":"left","width":"75%","padding":"10px","border":"1px solid #4f8a03","border-radius":"5px"} );
+			$("#oc-mod-reponses").css( {"float":"left","width":"50%"} );
+			$("#oc-mod-options").css( {"float":"left","width":"50%"} );
+			$("#oc-mod-valid").css( {"float":"left","width":"100%","text-align":"center"} );
+			$("#oc-mod-panel-title").css( {"color":"#4f8a03","font-weight":"bold","line-height":"1em","margin-bottom":"10px"} );
+			$("#oc-mod-reponses-title").css( {"color":"#000","font-weight":"bold","line-height":"1em"} );
+			$("#oc-mod-options-title").css( {"color":"#000","font-weight":"bold","line-height":"1em"} );
+			$("#oc-mod-options").append( '<input name="hasHeader" type="checkbox" value="1" /> Ajouter entête de réponse<br />' );
+			$("#oc-mod-options").append( '<input name="shouldLock" type="checkbox" value="1" /> Fermer le sujet 🔒<br />' );
+			$("#oc-mod-options").append( '<input name="postMessage" type="checkbox" checked="checked" value="1" /> Poster le message directement<br />' );
+			$("#oc-mod-options").append( '<input name="dismissAlerts" type="checkbox" value="1" /> Retirer les alertes<br />' );
+			$("#oc-mod-valid").append( '<a id="oc-mod-validation" class="btn btn-primary">Modérer</a>' );
+			$("#oc-mod-validation").css( {"margin":"10px 0 0 5px","border":"1px solid #380e00","box-shadow":"inset 0 1px 1px 0 #a95f47","background-color":"#691c02","background-image":"linear-gradient(to bottom,#872403 0,#763019 49%,#691c02 50%,#421100 100%)","text-shadow":"0 -1px 0 #1c181b","text-decoration":"none"} );
+			
+			// Ajout des messages possibles
+			for( let message of messagesSection ) {
+				$("#oc-mod-reponses").append( '<input class="oc-mod-checkboxes" type="checkbox" value="'+message.id+'" /> '+message.title+'<br />' );
+			}
+		}
+	}
+	
+	// Gestion de la mise à jour manuelle
+	$("#oc-mod-update").click( () => {
+		getConfigurationFile( true ).then( () => alert('Mise à jour des réponses effectuée !') );
+	});
+	
 	// Validation modération
-	$("#oc-mod-validation").click( function(e) {
-		var moderationMessage = '';
-
+	$("#oc-mod-validation").click( () => {
+		let moderationMessage = '';
+		
 		if( $("input[name=hasHeader]").prop('checked') )
-			moderationMessage += GM_getValue(answerFileIndex).configuration.headers;
-
+			moderationMessage += configuration.headers;
+		
 		$(".oc-mod-checkboxes").each( function(e) {
 			if( $(this).prop('checked') ) {
-				messageObject = GM_getValue(answerFileIndex).answers.filter( a => a.id == $(this).val() );
-				moderationMessage += messageObject[0].message;
+				let message = messages.filter( a => a.id == $(this).val() )[0].message;
+				moderationMessage += message;
 			}
 		});
-
+		
 		if( moderationMessage.length ) {
-
+			
 			// Retirer les alertes
 			if( $("input[name=dismissAlerts]").prop('checked') ) {
 				$(".span12>a").each( function(e) {
-                    var alertLink = baseUri + $(this).attr('href');
-                    promiseRequest("GET", alertLink )
-                        .then(() => console.log("Retrait alerte " + alertLink ) );
-                });
+					var alertLink = baseUri + $(this).attr('href');
+					promiseRequest("GET", alertLink )
+						.then(() => console.log("Retrait alerte " + alertLink ) );
+				});
 			}
-
-			// Fermeture du sujet
-			if( $("input[name=shouldLock]").prop('checked') ) {
-                var closeLink = baseUri + $(".closeAction").attr('href');
-                promiseRequest("GET", closeLink )
-                    .then(() => console.log("Fermeture sujet " + closeLink ) );
-            }
-
-			addMessage(moderationMessage);
 			
+			// Gestion fermeture du sujet
+			if( $("input[name=shouldLock]").prop('checked') )
+				GM_setValue( "threadToLock", baseUri + $(".closeAction").attr('href') );
+			else
+				GM_setValue( "threadToLock", '' );
+			
+			// Ajout du message dans l'éditeur
+			var textareaHolder = $("#Comment_wysiwyg_message_ifr");
+			if(textareaHolder.length)
+				textareaHolder[0].contentDocument.body.innerHTML = moderationMessage;
+			else
+				$("#Comment_wysiwyg_message")[0].value = moderationMessage;
+			
+			// Validation du formulaire si demandée
 			if( $("input[name=postMessage]").prop('checked') )
 				$("input[name=submit_comment]").click();
-
 		} else {
 			alert( 'Aucun message à poster !' );
 		}
 	});
-
-	/**
-	 * Ajoute le message dans le textarea
-	 *
-	 * @param {any} message
-	 */
-	function addMessage(message) {
-		let textareaHolder = $("#Comment_wysiwyg_message_ifr");
-
-		if(textareaHolder.length)
-			textareaHolder[0].contentDocument.body.innerHTML = message;
-		else
-			$("#Comment_wysiwyg_message")[0].value = message;
-	}
-
-	/**
-	 * Récupère la liste des messages de modération
-	 * pour la section courante ou son parent ou "all"
-	 * en excluant les sections précisées
-	 *
-	 * @returns Liste d'objet de réponses
-	 */
-	function getMessageBySection( messages, section ) {
-		var forum = false;
-		var retour = [];
-
-		for( var titre in forums ) {
-			if( $.inArray( section, forums[titre] ) > -1 )
-				forum = titre;
-		}
-
-		for( var i = 0; i < messages.length; i++) {
-			var sections = messages[i].section;
-			var excludes = messages[i].exclude;
-
-			if( $.inArray( section, excludes ) > -1 || $.inArray( forum, excludes ) > -1 || $.inArray( "all", excludes ) > -1 )
-				break;
-
-			if( $.inArray( section, sections ) > - 1 || $.inArray( forum, sections ) > -1 || $.inArray( "all", sections ) > -1 )
-				retour.push( messages[i] );
-		}
-
-		return retour;
-	}
-
+	
 	/**
 	 * Récupère le fichier de configuration du serveur si la dernière mise à jour
 	 * date de plus de 24 heures. Retourne une promesse pour poursuivre l'execution
@@ -215,20 +194,15 @@
 	 * @returns Promise avec les valeurs
 	 */
 	function getConfigurationFile(forceCheck) {
-		if(forceCheck || (GM_getValue(answerFileLastFetchIndex) === undefined || GM_getValue(answerFileLastFetchIndex) + delayBetweenConfigurationFetch > Date.now())) {
+		if( forceCheck || (GM_getValue("answersLastFetch") === undefined || GM_getValue("answersLastFetch") + 86400000 > Date.now())) {
 			return promiseRequest("GET", answerFileLink)
-				.then(response => GM_setValue(answerFileIndex, JSON.parse(response.responseText)))
-				.then(() => GM_setValue(answerFileLastFetchIndex, Date.now()));
+				.then(response => GM_setValue("answers", JSON.parse(response.responseText)))
+				.then(() => GM_setValue("answersLastFetch", Date.now()));
 		}
-
+		
 		return new Promise((resolve, reject) => resolve());
 	}
-
-	// Gestion de la mise à jour manuelle
-	$("#secondMenu").on('click', '#updateReply', () => {
-		getConfigurationFile(true).then(() => alert('Update done'));
-	});
-
+	
 	/**
 	 * Crée une XML request sous forme de promise
 	 *
@@ -246,3 +220,34 @@
 			});
 		});
 	}
+
+	/**
+	 * Récupère la liste des messages de modération
+	 * pour la section courante ou son parent ou "all"
+	 * en excluant les sections précisées
+	 *
+	 * @returns Liste d'objet de réponses
+	 */
+	function getMessageBySection( messages, section ) {
+		var forum = false;
+		var retour = [];
+		
+		for( var titre in forums ) {
+			if( $.inArray( section, forums[titre] ) > -1 )
+				forum = titre;
+		}
+		
+		for( var i = 0; i < messages.length; i++) {
+			var sections = messages[i].section;
+			var excludes = messages[i].exclude;
+			
+			if( $.inArray( section, excludes ) > -1 || $.inArray( forum, excludes ) > -1 || $.inArray( "all", excludes ) > -1 )
+				break;
+			
+			if( $.inArray( section, sections ) > - 1 || $.inArray( forum, sections ) > -1 || $.inArray( "all", sections ) > -1 )
+				retour.push( messages[i] );
+		}
+
+		return retour;
+	}
+	
