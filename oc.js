8fc1d54edec81592803930b537b4229cda37a2dd
benzouye <benzouye@hotmail.com>
2018-03-28 15:59:41 +0200

    Correction cursor bouton top/bottom

--- oc.js
+++ oc.js
@@ -9,7 +9,7 @@
 // @include			*openclassrooms.com/mp/*
 // @include			*openclassrooms.com/interventions/*
 // @include			*openclassrooms.com/sujets/*
-// @version			1.7.3
+// @version			1.7.4
 // @grant			GM_xmlhttpRequest
 // @grant			GM_getValue
 // @grant			GM_setValue
@@ -144,7 +144,7 @@ $("#oc-mod-bottom").click( () => {
 
 // Style bouton top/bottom
 $(".oc-mod-nav").css({
-	"pointer":"cursor",
+	"cursor":"pointer",
 	"position":"fixed",
 	"right":"50px",
 	"background":"#4f8a03",
