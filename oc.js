151fe466699363807a1a6828d29098d72ef0668c
benzouye <benzouye@hotmail.com>
2018-05-07 17:05:20 +0200

    v1.9.16

--- oc.js
+++ oc.js
@@ -9,7 +9,7 @@
 // @include			*openclassrooms.com/mp/*
 // @include			*openclassrooms.com/interventions/*
 // @include			*openclassrooms.com/sujets/*
-// @version			1.9.15
+// @version			1.9.16
 // @noframes
 // @grant			GM_xmlhttpRequest
 // @grant			GM_getValue
