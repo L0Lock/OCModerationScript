6cd13e49765645c10281b8f1d3fc5ff76c771edc
benzouye <benzouye@hotmail.com>
2019-02-26 15:48:04 +0100

    Position et couleur du bouton "suppr alertes"

--- oc.js
+++ oc.js
@@ -9,7 +9,7 @@
 // @include			*openclassrooms.com/*mp/*
 // @include			*openclassrooms.com/interventions/*
 // @include			*openclassrooms.com/sujets/*
-// @version			2.9.3
+// @version			2.9.4
 // @noframes
 // @grant			GM_xmlhttpRequest
 // @grant			GM_getValue
@@ -80,13 +80,13 @@
 
 	// ajout bouton suppression alertes
 	if( $(".span12>a").length ) {
-		$("#mainContentWithHeader").append('<span title="Retirer les alertes de la page" class="oc-mod-tooltip button--primary" id="oc-mod-dismiss"><i class="icon-garbage"></i></span>');
+		$("#mainContentWithHeader").append('<span title="Retirer les alertes de la page" class="oc-mod-tooltip button--warning" id="oc-mod-dismiss"><i class="icon-garbage"></i></span>');
 		$("#oc-mod-dismiss").click( dismissPageAlerts() );
 		$("#oc-mod-dismiss").css({
 			"cursor":"pointer",
 			"position":"fixed",
-			"left":"50px",
-			"top":"50%"
+			"left":"290px",
+			"top":"230px"
 		});
 	}
 
